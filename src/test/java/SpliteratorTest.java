26e764c902b90d09872e36b889f04d0908bc6d79
Alex T <alex.tenghamn@gmail.com>
2025-11-14 22:52:07 +0100

    more spliterator

--- src/test/java/SpliteratorTest.java
+++ src/test/java/SpliteratorTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Spliterator;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,7 +26,7 @@ public class SpliteratorTest {
     }
 
     @Test
-    public void givebAStreamOfArticles_whenProcessedUsingTrySplit_thenSplitIntoEqualHalf(){
+    public void givennAStreamOfArticles_whenProcessedUsingTrySplit_thenSplitIntoEqualHalf(){
         var articles = Stream.generate(() -> new Article( "An article"))
                 .limit(1000000)
                 .toList();
@@ -39,5 +42,45 @@ public class SpliteratorTest {
         assertEquals(500000, articlesListOne.size());
         assertEquals(500000,articlesListTwo.size());
         assertFalse(articlesListOne.containsAll(articlesListTwo));
+    }
+
+    @Test
+    public void givenAStreamOfIntegers_whenProcessedSequentialCustomSpliterator_countProducesRightOutput(){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        CustomSpliterator customSpliterator = new CustomSpliterator(numbers);
+        AtomicInteger sum = new AtomicInteger();
+
+        customSpliterator.forEachRemaining(sum::addAndGet);
+        assertEquals(15, sum.get());
+    }
+
+    @Test
+    public void givenAStreamOfIntegers_whenProcessedInParallelWithCustomSpliterator_countProducesRightOutput(){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        CustomSpliterator customSpliterator = new CustomSpliterator(numbers);
+
+        // ForkJoinPool for parallel processing
+        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+        AtomicInteger sum = new AtomicInteger(0);
+
+        // Process in parallel
+        forkJoinPool.submit(() -> {
+            Stream<Integer> parallelStream = StreamSupport.stream(customSpliterator, true);
+            parallelStream.forEach(sum::addAndGet);
+
+        }).join();
+        assertEquals(15, sum.get());
     }
 }
